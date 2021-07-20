87dfcfb8fce7a823b748225a82672758676dccbd
csslick <digitone@naver.com>
2021-07-20 15:41:30 +0900

    Add files via upload

---
+++ main-v.js
@@ -0,0 +1,29 @@
+// Vanilla JS
+
+window.onload = function() {
+
+  const dt = document.querySelectorAll('.faq dt');
+
+  for(let i = 0; i < dt.length; i++) {
+    dt[i].addEventListener('click', function(){
+      for(let j = 0; j < dt.length; j++){
+        if(i == j) continue;
+        dt[j].nextElementSibling.classList.remove('active');
+      }
+      this.nextElementSibling.classList.toggle('active');
+
+      if(this.nextElementSibling.classList.contains('active')) {
+        // 열렸을 때
+        const btn = document.querySelectorAll('.faq dt button');
+        for(let i = 0; i < btn.length; i++) {
+          btn[i].classList.remove('up');
+        }
+        console.log(this.children[0])
+        this.children[0].classList.add('up')
+      } else {
+        this.children[0].classList.remove('up')
+      }
+    });
+  }
+
+}
